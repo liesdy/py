33e06efe0e6439aa59dfe518e2467d76bcc782ed
洌水剑 <411476852@qq.com>
2019-12-31 17:29:56 +0800

    init

---
+++ 1.py语言基础/学习笔记/test/test.py
@@ -0,0 +1,55 @@
+# list = [1, 2, 3]
+# print(list[-1])
+# print(list[0:2])
+# print(list[1:])
+# print(list[:2])
+# print(list[ : : 2])
+
+# list = [1, 2, 3] + [4, 5, 6]
+# print(list)
+
+# list = [2, 8, 9, 225, 7]
+# print(max(list))
+# del list[2]
+# print(list)
+
+# list = [1, 2, 3, 4, 5]
+# list[0: 2] = 'swk'
+# print(list)
+
+# 参数测试
+
+
+# def fn3(b, c, e, *a, **d):
+#     print('a =', a, type(a))
+#     print('d =', d, type(d))
+#     print('b =', b)
+#     print('c =', c)
+
+
+# fn3(b=1, d=2, c=3, e=10, f=20)
+# fn3(1, 2, 3, 99, f=20)
+
+# def fun(numb):
+#     list = [1]
+#     i = 1
+#     n = 1
+#     while i < numb:
+#         list.append(i)
+#         i += list[n-1]
+#         n += 1
+#     return list
+
+
+# print(fun(10000))
+# def fun(i):
+#     a = 1
+#     b = 0
+#     for i in range(i):
+#         n = a
+#         a += b
+#         b = n
+#     return n
+
+
+# print(fun(10))
